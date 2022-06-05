52471888031c572a891c5718c6002c0aa659ef51
AadishJain249 <87666139+AadishJain249@users.noreply.github.com>
2022-06-05 15:55:51 +0530

    third

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,9 +1,9 @@
-const msgform = document.querySelector('.form')
-const msginput = document.querySelector('input')
-const msgbutton = document.querySelector('button')
-const messages = document.querySelector('.messages')
+const msgform = document.querySelector('#form')
+const msginput = document.querySelector('#input')
+const msgbutton = document.querySelector('#button')
+const messages = document.querySelector('#messages')
 const msgtemplate = document.querySelector('.msg-template').innerHTML
-const locations = document.querySelector('.location')
+const locations = document.querySelector('#location')
 const loctemplate = document.querySelector('.loc-template').innerHTML
 const socket1 = io()
 socket1.on('Message', (message) => {
@@ -14,15 +14,15 @@ socket1.on('Message', (message) => {
     })
     messages.insertAdjacentHTML('beforeend', html)
 })
-document.querySelector('.form').addEventListener('submit', function(e) {
+document.querySelector('#form').addEventListener('submit', function(e) {
     // When submitting a form, the browser sends a request to the server and refreshes the page. 
     // To disable this behavior, you can use event.preventDefault() when clicking the button
     e.preventDefault();
     const msg = document.querySelector('input').value
-    msgbutton.setAttribute('disabled', 'disabled')
+        // msgbutton.setAttribute('disabled', 'disabled')
     socket1.emit('SendMessage', msg, (error) => {
-        msgbutton.removeAttribute('disabled')
         msginput.value = ''
+        msgbutton.removeAttribute('disabled')
         msginput.focus()
         if (error) {
             return console.log(error);
@@ -30,8 +30,8 @@ document.querySelector('.form').addEventListener('submit', function(e) {
         console.log('message is delivered');
     })
 })
-const locbutton = document.querySelector('.location')
-document.querySelector('.location').addEventListener('click', function(e) {
+const locbutton = document.querySelector('#location')
+document.querySelector('#location').addEventListener('click', function(e) {
     e.preventDefault()
     locbutton.setAttribute('disabled', 'disabled')
     if (!navigator.geolocation) {
